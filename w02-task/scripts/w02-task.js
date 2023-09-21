a8102d1867432292d5df941ed6a351196b61d2ee
OMoncada <132164886+OMoncada@users.noreply.github.com>
2023-09-20 23:20:40 -0300

    Update w02-task.js

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,28 +1,36 @@
-// Reemplaza 'Tu Nombre' con tu nombre real.
-let fullName = 'Oscar Moncada';
+/* W02-Task - Profile Home Page */
 
-let currentYear = new Date().getFullYear();
+/* Step 1 - Setup type tasks - no code required */
 
-// Asegúrate de tener una imagen en la carpeta "images" y reemplaza 'tuimagen.png' con el nombre de tu imagen.
-let profilePicture = 'images/Person.png'; 
+/* Step 2 - Variables */
+const fullName = "Oscar A. Moncada";
+let currentYear = 2023;
+let profilePicture = "images/Person.png";
 
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('#profile-image');
 
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+
+/* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-foodElement.textContent = 'Tus comidas favoritas:';
 yearElement.textContent = currentYear;
-imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile Image of ${fullName}`);
 
-let favFoods = ['Pizza', 'Sushi', 'Burgers','Pizza', 'Sushi', 'Burgers','Pizza', 'Sushi', 'Burgers']; // Agrega tus comidas favoritas aquí.
 
-favFoods.push('Pasta'); // Agrega otra comida favorita.
 
-favFoods.shift(); // Elimina el primer elemento de la matriz.
-favFoods.pop(); // Elimina el último elemento de la matriz.
 
-foodElement.innerHTML += '<br>' + favFoods.join('<br>');
-
+/* Step 5 - Array */
+favoriteFoods = ["Rice", "Meat with vegetables", "Salad", "lentils", "Chips"];
+foodElement.textContent = favoriteFoods;
+addFavFood = "chocolate cakes";
+favoriteFoods.push(addFavFood);
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br> ${favoriteFoods}`;
